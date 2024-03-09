931b92abcd62a9aeac7b197dbce9043e212c5a90
xdznHexin <2387740264@qq.com>
2024-03-09 08:39:34 +0800

    feat：创建 SpringBoot3 + MyBatisPlus + MySQL 5.7 + Redis + SaToken模版

--- src/main/java/com/code/template/TemplateApplication.java
+++ src/main/java/com/code/template/TemplateApplication.java
@@ -3,13 +3,15 @@ package com.code.template;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * 模板应用程序
+ *
+ * @author HeXin
+ */
 @MapperScan("com.code.template.mapper")
 @EnableSwagger2
-@EnableScheduling
 @SpringBootApplication
 public class TemplateApplication {
 
